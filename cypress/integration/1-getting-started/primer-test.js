39cff4436021ca46fd0a0b26a4a1692330601999
Nicolas Lopez <81532585+Nicolopez603@users.noreply.github.com>
2023-10-16 00:08:32 -0300

    add new test
    adding new test with a interesting functionality

--- cypress/integration/1-getting-started/primer-test.js
+++ cypress/integration/1-getting-started/primer-test.js
@@ -213,4 +213,36 @@ describe("Central de Atendimento ao Cliente TAT", function () {
     cy.get("a").invoke("removeAttr", "target").click();
     cy.get("#title").should("have.text", "CAC TAT - Política de privacidade");
   });
+
+  it("  verificam mensagens (de sucesso e error", function () {
+    cy.clock();
+    cy.contains("button", "Enviar").click();
+    cy.get(".error").should("be.visible");
+    cy.tick(3000);
+    cy.get(".error").should("not.be.visible");
+  });
+
+  it("exibe e esconde as mensagens de sucesso e erro usando o .invoke", () => {
+    cy.get(".success")
+      .should("not.be.visible")
+      .invoke("show")
+      .should("be.visible")
+      .and("contain", "Mensagem enviada com sucesso.")
+      .invoke("hide")
+      .should("not.be.visible");
+    cy.get(".error")
+      .should("not.be.visible")
+      .invoke("show")
+      .should("be.visible")
+      .and("contain", "Valide os campos obrigatórios!")
+      .invoke("hide")
+      .should("not.be.visible");
+  });
+  it.only("preenche a area de texto usando o comando invoke", function () {
+    const longText = Cypress._.repeat("0123456789", 20);
+
+    cy.get("#open-text-area")
+      .invoke("val", longText)
+      .should("have.value", longText);
+  });
 });
